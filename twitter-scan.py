0fe3447d229d3ddb0f5c066d2054fe7d1586c7de
e <goldmouse67>
2017-03-04 08:43:50 -0500

    Update config info

--- twitter-scan.py
+++ twitter-scan.py
@@ -12,8 +12,8 @@ import random
 import sys
 
 # Variables that contains the user credentials to access Twitter API
-access_token = "2871836001-BGKrtPGjZrvAyI0CyD97dj035IiVyZvVmpaEU2f" #"793165501614678016-TCIFNBzYM70EH0AQTCObrHBUTAjKair"
-access_token_secret = "NGShxSG9v43vgv5Z8KSuwA3YeIllPFHnGPFl8FWcp4YO5" #
+access_token = "2871836001-1sPhtUEuUGoWGct7v6tFwUOspYpY9ROP2A8K0dQ" #"793165501614678016-TCIFNBzYM70EH0AQTCObrHBUTAjKair"
+access_token_secret = "uJjRdJTAyY39947QnumPWZpbQnBmYldYhmgusW8LemZx6" #
 consumer_key = "wdaBHumoamxEdAjAmEc4KYo8N" #" "
 consumer_secret = "6zu64D3Lg0EGPxnhyj5OxMuHX6wzQAFWW9US2xF8NYSqdcEfKx" #
 tweet_count = 0
