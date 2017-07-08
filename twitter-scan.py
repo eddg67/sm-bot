7679aac3b7f5674f846846b920839a659f67a94a
e <goldmouse67>
2017-07-08 11:10:30 -0400

    Update config info

--- twitter-scan.py
+++ twitter-scan.py
@@ -279,7 +279,7 @@ def process_stream():
     stream = Stream(auth, l)
     api = API(auth)
     set_trends()
-    stream.filter(track=track, language='en')
+    stream.filter(track=track, languages=['en'])
 
 
 # This is a basic listener that just prints received tweets to stdout.
